961e17518f319d1bfb77e69750c1acf0fb201586
Joshua Hintze <joshua.hintze@gmail.com>
2023-02-12 21:02:22 -0700

    initial bug

---
+++ common/test.ts
@@ -0,0 +1,6 @@
+export interface Test {
+    value: string;
+}
+
+let test: Test = { value: 'test'};
+export default test;
---
+++ electron/main/other.ts
@@ -0,0 +1,3 @@
+import test from "../../common/test";
+
+console.log(test);
